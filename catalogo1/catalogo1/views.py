838fa5dc5f6f2f206a00d7dc19992857d6d124e3
RickyRicardo19 <119453511+RickyRicardo19@users.noreply.github.com>
2022-12-17 20:05:32 -0300

    nuevo proyecto2

--- catalogo1/catalogo1/views.py
+++ catalogo1/catalogo1/views.py
@@ -1,7 +1,23 @@
 from django.shortcuts import render 
 
+from producto.models import Producto
+
 def inicio(request):
     template_name = "index.html"
+
+
+    #--------------------------------------------------
+    #query en django utilizando orm
+    """
+    p = Producto.objects.create(
+    nombre = "pantalon",
+    precio = "2000",
+    descripcion = "pantalon azul",
+    )
+    """
+    productos = Producto.objects.all()
+    print(productos)
     contexto = {
+        "productos": productos
     }
     return render(request, template_name, contexto)
